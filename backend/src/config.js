060e72c4767b1e1f35ec2ad6be9c9b6e7f0dbdac
Scott McPherson <s@smc.re>
2022-03-23 19:50:44 -0400

    edit

--- backend/src/config.js
+++ backend/src/config.js
@@ -65,7 +65,7 @@ const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a singl
 const PUBLIC_MINT_START_DATE = "2022-04-14T04:00:00+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = "2022-03-22T04:00:00+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = "2022-03-20T00:00:00+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0xE9034865393A7Ae897c0501596e3B6e37e32baff"; // Address that will receive the royalty
 const BASE_URI = null // only update if you want to manually set the base uri
